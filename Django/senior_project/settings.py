3f46abef02ee37ac3003672e04706714b7d9b205
mgdgit <mauriciogarza010@gmail.com>
2026-09-20 18:45:15 -0500

    Updating settings.py to recents PR

--- Django/senior_project/settings.py
+++ Django/senior_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
